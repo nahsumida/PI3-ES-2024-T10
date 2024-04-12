eab89de6ad6fde30d7246a1483ecf12c748b62a3
Natália Naomi Sumida <naah.sumida@gmail.com>
2024-04-12 06:38:43 -0300

    functiions example

--- safepack-functions/functions/src/index.ts
+++ safepack-functions/functions/src/index.ts
@@ -1,19 +1,66 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+const app = admin.initializeApp();
+const db = app.firestore();
+// const colUnidadeLocacao = db.collection("unidadeLocacao");
+const colPessoa = db.collection("pessoa");
+// const colLocacao = db.collection("locacao");
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+export const addSamplePerson = functions
+  .region("southamerica-east1")
+  .https.onRequest(async (request, response) => {
+    const pessoa = {
+      nome: "natalia firebase",
+      dataNascimento: "16 de abril de 2024 às 00:00:00 UTC-3",
+      ehGerente: false,
+      email: "pi@gmail.com",
+      cpf: "12312312345",
+      senha: "augdfau",
+      telefone: "99999999",
+    };
+    try {
+      const docRef = await colPessoa.add(pessoa);
+      response.send("pessoa inserida com sucesso. Referencia: " + docRef.id);
+    } catch (e) {
+      functions.logger.error("Erro ao inserir pessoa de exemplo");
+    }
+  });
 
-export const helloWorld = onRequest((request, response) => {
-    logger.info("Hello logs!", {structuredData: true});
-    response.send("Hello from Firebase!");
-});
+export const deletePerson = functions
+  .region("southamerica-east1")
+  .https.onRequest(async (request, response) => {
+    try {
+      const pessoaId = "7k9WSM8ISZg23ka1aNTX";
+      await colPessoa.doc(pessoaId).delete();
+      response.send("pessoa deletada com sucesso.");
+    } catch (e) {
+      functions.logger.error("Erro ao deletar pessoa");
+    }
+  });
+
+// buscar pessoa com email x
+export const searchPerson = functions
+  .region("southamerica-east1")
+  .https.onRequest(async (request, response) => {
+    if (request.method !== "GET") {
+      response.status(405).send("Metodo http não permitido");
+    }
+
+    try {
+      // chamado de snapshot, pois eles podem ser alterados
+      // simultaneamente, ou seja temos uma
+      // copia de como estavam no momento da consulta
+      const snapshot = await colPessoa
+        .where("email", "==", "pi@gmail.com")
+        .get();
+      const pessoa : any = [];
+      snapshot.forEach((doc) => {
+        pessoa.push(doc.data());
+      });
+      response.status(200)
+        .json(pessoa);
+    } catch (e) {
+      functions.logger.error("Erro ao buscar pessoa");
+    }
+  });
