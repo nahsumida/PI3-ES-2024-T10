bb495090fed4fab0d6205c4f375e07f5a9f61fab
gp_aranha <gianp_aranha@hotmail.com>
2024-04-15 14:35:50 -0300

    add google maps activity

--- safepack/app/src/main/java/br/edu/puccampinas/safepack/MapsActivity.kt
+++ safepack/app/src/main/java/br/edu/puccampinas/safepack/MapsActivity.kt
@@ -15,6 +15,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private var binding: ActivityMapsBinding? = null
+    private val places = arrayListOf(
+        Place("PUCC Campus 1", LatLng(-22.8360456,-47.0564085),
+            "Av. Reitor Benedito José Barreto Fonseca, H15 - Parque dos Jacarandás, Campinas - SP"),
+        Place("Oxxo PUCC", LatLng(-22.8363415,-47.0531125),
+            "Av. Profa. Ana Maria Silvestre Adade, 607 - Parque das Universidades, Campinas - SP, 13086-130")
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,24 +31,37 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        //mapFragment.getMapAsync(this)
+        mapFragment.getMapAsync { googleMap ->
+            addMarkers(googleMap)
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    private fun addMarkers(googleMap: GoogleMap) {
+        places.forEach { place ->
+            val marker = googleMap.addMarker(
+                MarkerOptions()
+                    .title(place.name)
+                    .position(place.latLng)
+                    .snippet(place.address)
+            )
+        }
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(places[0].latLng))
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val pucc = LatLng(-22.8360456,-47.0564085)
+        mMap.addMarker(MarkerOptions().position(pucc).title("Marcador na PUCC"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pucc))
     }
+
+
 }
+data class Place (
+    val name: String,
+    val latLng: LatLng,
+    val address: String,
+)
