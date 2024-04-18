637fc4ed337d8a068854c9025eac8d3515bb9e29
Natália Naomi Sumida <naah.sumida@gmail.com>
2024-04-18 13:43:26 -0300

    alugar armario with constraint

--- safepack/app/src/main/java/br/edu/puccampinas/safepack/AlugarArmarioActivity.kt
+++ safepack/app/src/main/java/br/edu/puccampinas/safepack/AlugarArmarioActivity.kt
@@ -2,10 +2,7 @@ package br.edu.puccampinas.safepack
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import br.edu.puccampinas.safepack.databinding.ActivityAlugarArmarioBinding
 
 class AlugarArmarioActivity : AppCompatActivity() {
--- safepack/app/src/main/java/br/edu/puccampinas/safepack/InfoArmarioActivity.kt
+++ safepack/app/src/main/java/br/edu/puccampinas/safepack/InfoArmarioActivity.kt
@@ -4,10 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import br.edu.puccampinas.safepack.databinding.ActivityInfoArmarioBinding
 
 class InfoArmarioActivity : AppCompatActivity()  {
